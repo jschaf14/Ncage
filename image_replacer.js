f01cf6b55ed926e03f32b073a1c405bbe0a31dcb
riley <parish.riley@gmail.com>
2020-11-09 23:26:28 -0700

    Added additional element attributes to our replacement images so it's more robust.

--- image_replacer.js
+++ image_replacer.js
@@ -10,17 +10,21 @@ chrome.storage.sync.get('imgReplaceProb', function(data) {
 window.onload = function () {
     // get an array of all the image elements
     var allImages = document.getElementsByTagName("img");
-
     // loop though that array of image elements
     for (var image of allImages) {
         if(shouldReplaceImg()) {
             // reset the image source
             image.src = "https://www.gstatic.com/tv/thumb/persons/258/258_v9_bb.jpg"
+            // some images have a "srcset" attribute instead of "src". Set both so it always works.
+            image.srcset = "https://www.gstatic.com/tv/thumb/persons/258/258_v9_bb.jpg"
+            // set the height and width of the new image to be the same as the old 
+            // (this is important if the original image doesn't have existing height and width attributes)
+            image.setAttribute("height",image.height)
+            image.setAttribute("width",image.width)
             image.setAttribute("style", "object-fit:cover;")
         }
     }
 }
-
 
 function shouldReplaceImg(){
     // generate a random number from 1 to 100
